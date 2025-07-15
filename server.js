c7663a79fe930e776b3ca9f3ecf44756149a153d
melegattip <melegattip@gmail.com>
2025-07-15 12:01:55 -0300

    Refactor: Remove unused _headers and netlify.toml files; add missing newline in server.js

--- server.js
+++ server.js
@@ -62,4 +62,4 @@ const server = http.createServer((req, res) => {
 // CRÍTICO: Escuchar en 0.0.0.0, no en localhost
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
-}); 
+});
